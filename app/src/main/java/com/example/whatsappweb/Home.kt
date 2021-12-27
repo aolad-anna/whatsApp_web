041993a2431d9b1627c0d523c05b70d974a83e86
Anna <anna.ecomclips@gmail.com>
2021-12-27 17:18:05 +0600

    fix

---
+++ app/src/main/java/com/example/whatsappweb/Home.kt
@@ -0,0 +1,32 @@
+package com.example.whatsappweb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class Home : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        val webView = findViewById<WebView>(R.id.whatsapp)
+        webView.settings.javaScriptEnabled = true
+        webView.settings.userAgentString = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:90.0) Gecko/20100101 Firefox/90.0"
+        webView.settings.loadsImagesAutomatically = true;
+        webView.settings.loadWithOverviewMode = true;
+        webView.settings.useWideViewPort = true;
+        webView.settings.domStorageEnabled = true;
+
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                if (url != null) {
+                    view?.loadUrl(url)
+                }
+                return true
+            }
+        }
+        webView.loadUrl("https://web.whatsapp.com/")
+    }
+}
--- app/src/main/java/com/example/whatsappweb/MainActivity.kt
+++ app/src/main/java/com/example/whatsappweb/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.whatsappweb
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.webkit.WebView
 import android.webkit.WebViewClient
 
@@ -12,23 +14,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val webView = findViewById<WebView>(R.id.whatsapp)
-        webView.settings.javaScriptEnabled = true
-        webView.settings.userAgentString = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:90.0) Gecko/20100101 Firefox/90.0"
-        webView.settings.loadsImagesAutomatically = true;
-        webView.settings.loadWithOverviewMode = true;
-        webView.settings.useWideViewPort = true;
-        webView.settings.domStorageEnabled = true;
-
-
-        webView.webViewClient = object : WebViewClient() {
-            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
-                if (url != null) {
-                    view?.loadUrl(url)
-                }
-                return true
-            }
-        }
-        webView.loadUrl("https://web.whatsapp.com/")
+        Handler().postDelayed(Runnable
+        { // This method will be executed once the timer is over
+            val i = Intent(this, Home::class.java)
+            startActivity(i)
+            finish()
+        }, 1500
+        )
     }
 }
